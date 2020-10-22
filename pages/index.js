8d7d8480847a98cc4cdc4520865c526ab3333543
Ito Hiroaki <h.ito@iseed.jp>
2020-10-22 18:40:05 +0900

    [update]titleに動的に値を渡す

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Layout from '../components/Layout';
+import GreetLink from '../components/GreetLink';
 
 export default function Index() {
   return (
     <>
       <Layout>
-      <p>こちらがHomeページ</p>
+      <h1>挨拶一覧</h1>
+      <ul>
+        <GreetLink title="おはようございます！" />
+        <GreetLink title="こんにちは！" />
+        <GreetLink title="こんばんは！" />
+      </ul>
       </Layout>
     </>
   );
